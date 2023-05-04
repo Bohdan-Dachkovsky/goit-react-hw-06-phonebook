b107f77cdc90fd3242d55429564f8b4b30e47ac1
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-05-05 01:38:24 +0300

    Add packages

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ContactForm from './components/ContactForm/ContactForm.jsx'
+import ContactList from './components/ContactList/ContactList.jsx'
+import Filter from './components/Filter/Filter.jsx'
+import { selectedPerson } from './redux/contacts/selectors.js'
+import { useSelector } from 'react-redux'
+
+export const App = () => {
+  const contacts = useSelector(selectedPerson)
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm />
+      {contacts?.length && <Filter />}
+      <ContactList />
+    </div>
+  )
+}
---
+++ src/redux/contacts/contactSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const contactSlice = createSlice({
+  name: 'user',
+  initialState: {
+    contacts: [
+      { id: 1, name: 'Rasel Dosh', number: '093-8383-355' },
+      { id: 2, name: 'Jonsion Resta', number: '093-8459-689' },
+      { id: 3, name: 'Boris Bankli', number: '095-4543-344' },
+      { id: 4, name: 'Eden Clements', number: '095-35434-905' },
+    ],
+    filter: '',
+  },
+  reducers: {
+    addUser(state, action) {
+      return { ...state, contacts: [action.payload], filter: '' };
+    },
+    dltContact(state, action) {
+      const deletedContacts = state.contacts.filter(
+        item => item.id !== action.payload
+      );
+      if (state.contacts?.id ?? true) {
+        state.contacts = deletedContacts;
+      }
+    },
+    searchContacts(state, action) {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const {
+  addUser,
+  dltContact,
+  searchContacts,
+  filteredContacts,
+} = contactSlice.actions;
+export default contactSlice.reducer;
