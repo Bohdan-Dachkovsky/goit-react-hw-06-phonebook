235be5baa2373d34e030ca0cc971ee7493f8e432
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-20 19:35:47 +0300

    Remove web-socket

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import ContactForm from './components/ContactForm/ContactForm.jsx'
 import ContactList from './components/ContactList/ContactList.jsx'
 import Filter from './components/Filter/Filter.jsx'
 import { selectedPerson } from './redux/contacts/selectors.js'
-import WebSocket from 'ws'
 import { useSelector } from 'react-redux'
 
-  const ws = new WebSocket('wss://localhost:3000');
 
-  ws.on('error', console.error);
+
 export const App = () => {
   const contacts = useSelector(selectedPerson)
   return (
